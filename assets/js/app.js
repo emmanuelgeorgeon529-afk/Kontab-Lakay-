c1d69def36a33931a4b478733f30baf6ca724d86
emmanuelgeorgeon529-afk <emmanuelgeorgeon529@gmail.com>
2026-07-02 12:10:15 -0400

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+// ==========================
+// KONTAPRO v2.0
+// APP.JS
+// ==========================
+
+const APP_NAME = "KontaPro";
+
+console.log(APP_NAME + " démarré.");
+
+function toast(message){
+
+    alert(message);
+
+}
