e3d59738ba980ec2ed7f3461ad582109c38182e8
emmanuelgeorgeon529-afk <emmanuelgeorgeon529@gmail.com>
2026-07-04 09:33:29 -0400

    Create firebase.js

---
+++ assets/js/assets/js/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTcbcnB3LMVCYefgMXwfk9czWF2uZc2Ps",
+  authDomain: "kontapro-f4066.firebaseapp.com",
+  projectId: "kontapro-f4066",
+  storageBucket: "kontapro-f4066.firebasestorage.app",
+  messagingSenderId: "613572396272",
+  appId: "1:613572396272:web:92c837d147da15fd81ae35",
+  measurementId: "G-YJT37DDDZ9"
+};
